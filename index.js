7b3e2e013523898e18ac6ed789ca8d3e2d5fe8b7
Douglas Scriptore <douglass@rpc.com.br>
2022-09-09 14:51:57 -0300

    chore: implement firebase

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 
+import '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
+
 import App from './App';
+
+// if (__DEV__) {
+//     firestore().terminate().then(() => {
+//         firestore().clearPersistence().then(() => {
+//             firestore().useEmulator('localhost', 8080);
+//         }).catch(() => {
+//             console.log("Clear persistence Error")
+//         })
+//     }).catch(() => {
+//         console.log("Terminate error")
+//     })
+//   }
+  
+// firestore();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import storage from "@react-native-firebase/storage"
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Photo } from '../../components/Photo';
 
 import { Container, Content, Progress, Transferred } from './styles';
+import { Alert } from 'react-native';
 
 export function Upload() {
   const [image, setImage] = useState('');
+  const [bytesTransferred, setBytesTransferred] = useState('');
+  const [progress, setProgress] = useState("0")
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -26,24 +30,49 @@ export function Upload() {
     }
   };
 
+  async function handleUpload() {
+    const filename = new Date().getTime()
+    const MIME = image.match(/\.(?:.(?!\.))+$/)
+    const reference = storage().ref(`/images/${filename}${MIME}`);
+
+    const uploadtask = reference.putFile(image)
+
+    uploadtask.on('state_changed', taskSnapshot => {
+      const percent = ((taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) * 100).toFixed(0)
+      setProgress(percent)
+      setBytesTransferred(`${taskSnapshot.bytesTransferred} transferido de ${taskSnapshot.totalBytes}`)
+    })
+
+    uploadtask.then(async () => {
+      const imageUrl = await reference.getDownloadURL()
+      Alert.alert('Imagem enviada com sucesso')
+    });
+    uploadtask.catch(error => console.log(error))
+    
+    // reference
+    //   .putFile(image)
+    //   .then(() => Alert.alert('Imagem enviada com sucesso'))
+    //   .catch(error => console.log(error))
+  }
+
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Upload de Fotos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
 
         <Button
           title="Fazer upload"
-          onPress={() => { }}
+          onPress={handleUpload}
         />
 
         <Progress>
-          0%
+          {progress}%
         </Progress>
 
         <Transferred>
-          0 de 100 bytes transferido
+          {bytesTransferred}
         </Transferred>
       </Content>
     </Container>
