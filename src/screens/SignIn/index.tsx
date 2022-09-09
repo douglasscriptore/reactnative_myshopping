d3478f689cb7d243f921402f87f1c05b73ea7291
Douglas Scriptore <douglass@rpc.com.br>
2022-09-09 16:13:53 -0300

    chore: implement firebase auth

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import auth from '@react-native-firebase/auth'
+
 
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import { Alert } from 'react-native';
 
 export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function handleSignInAnonymously(){
+    const { user } = await auth().signInAnonymously();
+  }
+
+  function handleCreateUserAccount(){
+    auth().createUserWithEmailAndPassword(email,password)
+    .then(() => {
+      Alert.alert('Usuario criado com sucesso')
+    }).catch(error => {
+      if(error.code === "auth/email-already-in-use"){
+        Alert.alert('Este e-mail já está em uso')
+      }
+
+      if(error.code === 'auth/invalid-email'){
+        Alert.alert('E-mail inválido')
+      }
+
+      if(error.code === 'auth/weak-password'){
+        Alert.alert('A senha deve ter ao menos 6 dígitos')
+
+      }
+    })
+
+  }
+
+  function handleLoginWithEmailAndPassword() {
+    auth()
+      .signInWithEmailAndPassword(email,password)
+      .then(({user}) => {
+
+      }).catch(error => {
+        if(error.code === 'auth/user-not-found' || error.code === "auth/wrong-password"){
+          Alert.alert('Usuário não encontrado. E-mail e/ou Senha inválido')
+        }
+      })
+
+  }
+
   return (
     <Container>
       <Title>MyShopping</Title>
@@ -14,18 +58,21 @@ export function SignIn() {
       <Input
         placeholder="e-mail"
         keyboardType="email-address"
+        onChangeText={setEmail}
       />
 
       <Input
         placeholder="senha"
         secureTextEntry
+        onChangeText={setPassword}
       />
 
       <Button title="Entrar" onPress={() => { }} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Recuperar senha" onPress={handleLoginWithEmailAndPassword} />
+        {/* {Apenas didatico, nao foi desenvolvido tela para criacao de conta} */}
+        <ButtonText title="Criar minha conta" onPress={handleCreateUserAccount} />
       </Account>
     </Container>
   );
